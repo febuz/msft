8a801e195f9c1f69c937e18228a1be7e92d488f7
$wittywildebeest-4698 <$wittywildebeest-4698>
2019-10-27 20:04:14 +0000

    Full flask

---
+++ FlaskApplication/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    text = { 'content': 'Welcome to eudemonia Azure PowerApp !' } 
+    return render_template("home.html",
+        title = 'Home',
+        text = text)
+
+
+app.debug = True
+if __name__ == "__main__":
+    app.run()
