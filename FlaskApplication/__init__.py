18b056945902c9313100028c5d60aabd2af65271
$wittywildebeest-4698 <$wittywildebeest-4698>
2019-10-27 20:07:30 +0000

    new

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    text = { 'content': 'Welcome to eudemonia Azure PowerApp !' } 
+    text = { 'content': 'Welcome to eudemonia 1 Azure PowerApp !' } 
     return render_template("home.html",
         title = 'Home',
         text = text)
